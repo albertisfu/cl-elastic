cb41c356b1f8944a898f19447acb53e62b462a3b
Alberto Islas <albertisfu@gmail.com>
2023-07-12 11:25:46 -0600

    fix(docker): Added security and SSL connection for Kibana

--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Fetching certificate..."
 aws secretsmanager get-secret-value --secret-id dev/certs/ca-cert --query SecretString --output text > /usr/share/kibana/config/certs/ca.crt
+aws secretsmanager get-secret-value --secret-id dev/certs/cl-kibana-key --query SecretString --output text > /usr/share/kibana/config/certs/cl-kibana.key
+aws secretsmanager get-secret-value --secret-id dev/certs/cl-kibana-crt --query SecretString --output text > /usr/share/kibana/config/certs/cl-kibana.crt
 
 # Start Kibana
 exec /usr/local/bin/kibana-docker
