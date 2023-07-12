205792f5deed79361551399b0489f23bcba83cfd
Alberto Islas <albertisfu@gmail.com>
2023-07-12 12:32:12 -0600

    fix(docker): Define secrets IDS from env vars

--- elastic/docker-entrypoint.sh
+++ elastic/docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+# Environment variables for secret ids
+ES_NODE_KEY_SECRET_ID=${ES_NODE_KEY_SECRET_ID:-"dev/certs/cl-es-node-key"}
+ES_NODE_CERT_SECRET_ID=${ES_NODE_CERT_SECRET_ID:-"dev/certs/cl-es-node-crt"}
+CA_CERT_SECRET_ID=${CA_CERT_SECRET_ID:-"dev/certs/ca-cert"}
+
 echo "Fetching certificates..."
-aws secretsmanager get-secret-value --secret-id dev/certs/cl-es-node-key --query SecretString --output text > /usr/share/elasticsearch/config/certs/cl-es-node.key
-aws secretsmanager get-secret-value --secret-id dev/certs/cl-es-node-crt --query SecretString --output text > /usr/share/elasticsearch/config/certs/cl-es-node.crt
-aws secretsmanager get-secret-value --secret-id dev/certs/ca-cert --query SecretString --output text > /usr/share/elasticsearch/config/certs/ca.crt
+aws secretsmanager get-secret-value --secret-id $ES_NODE_KEY_SECRET_ID --query SecretString --output text > /usr/share/elasticsearch/config/certs/cl-es-node.key
+aws secretsmanager get-secret-value --secret-id $ES_NODE_CERT_SECRET_ID --query SecretString --output text > /usr/share/elasticsearch/config/certs/cl-es-node.crt
+aws secretsmanager get-secret-value --secret-id $CA_CERT_SECRET_ID --query SecretString --output text > /usr/share/elasticsearch/config/certs/ca.crt
 
 # Start Elasticsearch as elasticsearch user
 echo "Starting Elasticsearch..."
--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "Fetching certificate..."
-aws secretsmanager get-secret-value --secret-id dev/certs/ca-cert --query SecretString --output text > /usr/share/kibana/config/certs/ca.crt
-aws secretsmanager get-secret-value --secret-id dev/certs/cl-kibana-key --query SecretString --output text > /usr/share/kibana/config/certs/cl-kibana.key
-aws secretsmanager get-secret-value --secret-id dev/certs/cl-kibana-crt --query SecretString --output text > /usr/share/kibana/config/certs/cl-kibana.crt
+# Environment variables for secret ids
+CA_CERT_SECRET_ID=${CA_CERT_SECRET_ID:-"dev/certs/ca-cert"}
+KIBANA_KEY_SECRET_ID=${KIBANA_KEY_SECRET_ID:-"dev/certs/cl-kibana-key"}
+KIBANA_CERT_SECRET_ID=${KIBANA_CERT_SECRET_ID:-"dev/certs/cl-kibana-crt"}
+
+echo "Fetching certificates..."
+aws secretsmanager get-secret-value --secret-id $CA_CERT_SECRET_ID --query SecretString --output text > /usr/share/kibana/config/certs/ca.crt
+aws secretsmanager get-secret-value --secret-id $KIBANA_KEY_SECRET_ID --query SecretString --output text > /usr/share/kibana/config/certs/cl-kibana.key
+aws secretsmanager get-secret-value --secret-id $KIBANA_CERT_SECRET_ID --query SecretString --output text > /usr/share/kibana/config/certs/cl-kibana.crt
 
 # Start Kibana
 exec /usr/local/bin/kibana-docker
