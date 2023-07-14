b5ca2adce2b609a62868472e37fc27cf64e8359c
Alberto Islas <albertisfu@gmail.com>
2023-07-14 14:31:48 -0600

    fix(docker): Remove AWS keys setup

--- elastic/docker-entrypoint.sh
+++ elastic/docker-entrypoint.sh
@@ -11,15 +11,6 @@ aws secretsmanager get-secret-value --secret-id $ES_NODE_KEY_SECRET_ID --query S
 aws secretsmanager get-secret-value --secret-id $ES_NODE_CERT_SECRET_ID --query SecretString --output text > /usr/share/elasticsearch/config/certs/cl-es-node.crt
 aws secretsmanager get-secret-value --secret-id $CA_CERT_SECRET_ID --query SecretString --output text > /usr/share/elasticsearch/config/certs/ca.crt
 
-# Create Elasticsearch keystore if it doesn't exist
-if [ ! -f /usr/share/elasticsearch/config/elasticsearch.keystore ]; then
-    /usr/share/elasticsearch/bin/elasticsearch-keystore create -p
-fi
-
-# Add settings to Elasticsearch keystore
-echo $AWS_ACCESS_KEY | /usr/share/elasticsearch/bin/elasticsearch-keystore add --stdin discovery.ec2.access_key
-echo $AWS_SECRET_KEY | /usr/share/elasticsearch/bin/elasticsearch-keystore add --stdin discovery.ec2.secret_key
-
 # Start Elasticsearch as elasticsearch user
 echo "Starting Elasticsearch..."
 exec /usr/share/elasticsearch/bin/elasticsearch
